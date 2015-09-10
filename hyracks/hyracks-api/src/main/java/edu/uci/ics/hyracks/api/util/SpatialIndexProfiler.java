af01554fc8870d358b97ee95093695912abc58d9
Young-Seok <kisskys@gmail.com>
2015-09-09 20:05:37 -0700

    added spaptial index profiler home dir

--- hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/util/SpatialIndexProfiler.java
+++ hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/util/SpatialIndexProfiler.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 
 public class SpatialIndexProfiler {
     public static final SpatialIndexProfiler INSTANCE = new SpatialIndexProfiler();
+    public static final String PROFILE_HOME_DIR = "/mnt/data/sdg/youngsk2/";
     public ExperimentProfiler shbtreeNumOfSearchPerQuery;
     public ExperimentProfiler dhbtreeNumOfSearchPerQuery;
     public ExperimentProfiler dhvbtreeNumOfSearchPerQuery;
@@ -18,24 +19,24 @@ public class SpatialIndexProfiler {
     private SpatialIndexProfiler() {
         if (ExperimentProfiler.PROFILE_MODE) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss.SSS");
-            shbtreeNumOfSearchPerQuery = new ExperimentProfiler("shbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            shbtreeNumOfSearchPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "shbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             shbtreeNumOfSearchPerQuery.begin();
-            dhbtreeNumOfSearchPerQuery = new ExperimentProfiler("dhbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            dhbtreeNumOfSearchPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "dhbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             dhbtreeNumOfSearchPerQuery.begin();
-            dhvbtreeNumOfSearchPerQuery = new ExperimentProfiler("dhvbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            dhvbtreeNumOfSearchPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "dhvbtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             dhvbtreeNumOfSearchPerQuery.begin();
-            rtreeNumOfSearchPerQuery = new ExperimentProfiler("rtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            rtreeNumOfSearchPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "rtreeNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             rtreeNumOfSearchPerQuery.begin();
-            sifNumOfSearchPerQuery = new ExperimentProfiler("sifNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            sifNumOfSearchPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "sifNumOfSearchPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             sifNumOfSearchPerQuery.begin();
             
-            falsePositivePerQuery = new ExperimentProfiler("falsePositivePerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            falsePositivePerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "falsePositivePerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             falsePositivePerQuery.begin();
-            cacheMissPerQuery = new ExperimentProfiler("cacheMissPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            cacheMissPerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "cacheMissPerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             cacheMissPerQuery.begin();
-            pidxSearchTimePerQuery = new ExperimentProfiler("pidxSearchTimePerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            pidxSearchTimePerQuery = new ExperimentProfiler(PROFILE_HOME_DIR + "pidxSearchTimePerQuery-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             pidxSearchTimePerQuery.begin();
-            sidxCPUCostProfiler = new ExperimentProfiler("sidxCPUCostProfiler-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
+            sidxCPUCostProfiler = new ExperimentProfiler(PROFILE_HOME_DIR + "sidxCPUCostProfiler-" + sdf.format(Calendar.getInstance().getTime()) + ".txt", 1);
             sidxCPUCostProfiler.begin();
         }
     }
